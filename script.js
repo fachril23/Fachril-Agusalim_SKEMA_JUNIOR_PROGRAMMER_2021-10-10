dcab86883eecf237178bba25761c85d722208be8
fachril23 <91230523+fachril23@users.noreply.github.com>
2021-10-10 12:41:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-const numbers = document.querySelectorAll(".number")
-console.log(numbers)
-
-numbers.forEach((number) => {
-    console.log(number)
-
-    number.addEventListener("click", () => {
-        console.log("number is pressed")
-    })
-
-    number.addEventListener("click", (event) => {
-        updateScreen(event.target.value)
-        inputNumber(event.target.value)
-        updateScreen(currentNumber)
-    })
-})
-
-const calculatorScreen = document.querySelector('.calculator-screen')
+const calculatorScreen = document.querySelector(".calculator-screen")
 
 const updateScreen = (number) => {
     calculatorScreen.value = number
 }
 
-let prevNumber = ''
-let calculatorOperator =''
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number)=> {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+let prevNumber =''
+let calculationOperator = ''
 let currentNumber ='0'
 
 const inputNumber = (number) => {
-    if (currentNumber === '0') {
+    if (currentNumber=== '0'){
         currentNumber = number
     } else {
         currentNumber += number
@@ -37,76 +29,84 @@ const operators = document.querySelectorAll(".operator")
 
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
-        console.log(event.target.value)
         inputOperator(event.target.value)
     })
 })
 
 const inputOperator = (operator) => {
-    if (calculatorOperator === '') {
+    if (calculationOperator === '') {
         prevNumber = currentNumber
     }
-    calculatorOperator = operator 
+    calculationOperator = operator
     currentNumber = ''
-
 }
 
 const equalSign = document.querySelector('.equal-sign')
 
 equalSign.addEventListener('click', () => {
-    console.log('equal button is pressed')
-    calculate ()
+    calculate()
     updateScreen(currentNumber)
 })
 
-const  calculate = () => {
+const calculate = () => {
     let result = ''
-    switch(calculatorOperator) {
-        case "+":
+    switch(calculationOperator){
+        case '+':
             result = parseFloat(prevNumber) + parseFloat(currentNumber)
             break
         case "-":
-            result = prevNumber - currentNumber
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
             break
         case "*":
-            result = prevNumber * currentNumber
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
             break
         case "/":
-            result = prevNumber / currentNumber
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
             break
-            default:
-                return
+        default:
+            return
     }
     currentNumber = result
-    calculatorOperator = ''
+    calculationOperator = '0'
+}
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
 }
 
 const clearBtn = document.querySelector('.all-clear')
 
 clearBtn.addEventListener('click', () => {
-    console.log('AC button is pressed')
-    clearAll ()
+    clearAll()
     updateScreen(currentNumber)
 })
 
-const clearAll = () => {
-    prevNumber = ''
-    calculatorOperator =''
-    currentNumber ='0'
-}
-
 const decimal = document.querySelector('.decimal')
 
-decimal.addEventListener('click', (event) => {
-    console.log(event.target.value)
+decimal.addEventListener('click', (event)=> {
     inputDecimal(event.target.value)
     updateScreen(currentNumber)
 })
 
 inputDecimal = (dot) => {
-    currentNumber += dot
     if(currentNumber.includes('.')) {
         return
     }
     currentNumber += dot
+}
+
+const percentage = document.querySelector('.percentage')
+
+percentage.addEventListener('click', (event)=> {
+    inputPercentage(event.target.value)
+    updateScreen(currentNumber)
+})
+
+inputPercentage = (per) => {
+    if(currentNumber.includes('%')) {
+        return 
+    }
+    currentNumber = currentNumber / 100 
 }
